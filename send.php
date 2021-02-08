22fda62d66d154dd47456686c62ed7933a63c149
Ghoudan Ayoub <ayoubghoudanos@gmail.com>
2021-02-08 13:59:04 +0100

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 $ch=curl_init("https://fcm.googleapis.com/fcm/send");
-$header=array("Content-Type:application/json","Authorization: key=AAAAnSVv7kU:APA91bErWb1Ps-29el8QjW-UdQA21QNcvDyf8yBHJBpJWhNA140K7Xr0JysE4--eLlI9NK0721-s53Hqp6uHPzafQ9sd_EvWBGt-N5ymSuy0H0paEl4xJZG-sbh1jDufr702fTzbYFBn");
+$header=array("Content-Type:application/json","Authorization: key=<KEY_APP>");
 
 
 $data=json_encode(array("to"=>$_REQUEST['to'],"notification"=>array("title"=>$_REQUEST['title'],"body"=>$_REQUEST['body'])));
