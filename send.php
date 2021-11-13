0d07f3bff06ebb9df1f7041ad9c44eeb7b3a7e0a
ayoub <ayoubghoudanos@gmail.com>
2021-11-13 16:29:12 +0100

    new addrr

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 $ch=curl_init("https://fcm.googleapis.com/fcm/send");
-$header=array("Content-Type:application/json","Authorization: key=<KEY_APP>");
+$header=array("Content-Type:application/json","Authorization: key=AAAAnSVv7kU:APA91bErWb1Ps-29el8QjW-UdQA21QNcvDyf8yBHJBpJWhNA140K7Xr0JysE4--eLlI9NK0721-s53Hqp6uHPzafQ9sd_EvWBGt-N5ymSuy0H0paEl4xJZG-sbh1jDufr702fTzbYFBn");
 
 
 $data=json_encode(array("to"=>$_REQUEST['to'],"notification"=>array("title"=>$_REQUEST['title'],"body"=>$_REQUEST['body'])));
@@ -9,4 +9,3 @@ curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
 curl_setopt($ch,CURLOPT_POST,1);
 curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
 curl_exec($ch);
-?>
